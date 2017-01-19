544d297067ef01bd06a383d33987447b4982281a
Matous Jobanek <mjobanek@redhat.com>
2017-01-19 13:39:27 +0000

    refactoring and added comments in SectionTree

--- core/reporter-impl/src/main/java/org/arquillian/reporter/impl/SectionTree.java
+++ core/reporter-impl/src/main/java/org/arquillian/reporter/impl/SectionTree.java
@@ -1,9 +1,6 @@
 package org.arquillian.reporter.impl;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -56,75 +53,98 @@ public class SectionTree<SECTIONTYPE extends SectionEvent<SECTIONTYPE, PAYLOAD_T
     }
 
     public void merge(SectionTree<SECTIONTYPE, PAYLOAD_TYPE> treeToMerge) {
+        // merge this tree
         associatedReport.merge(treeToMerge.getAssociatedReport());
 
+        // if there is some subtree to merge try to find corresponding subtree in the current tree
         if (!treeToMerge.getSubtrees().isEmpty()) {
 
             SectionTree subtreeToMerge = treeToMerge.getSubtrees().get(0);
             Identifier rootIdentifierSubtreeToMerge = subtreeToMerge.getRootIdentifier();
-            SectionTree matchedTree = null;
+            SectionTree matchedSubtree = null;
 
+            // check if current tree has any subtree
             if (!getSubtrees().isEmpty()) {
-                if (Validate.isNotEmpty(rootIdentifierSubtreeToMerge.getSectionId())) {
-                    List<SectionTree> filtered = getSubtrees()
-                        .stream()
-                        .filter(tree -> tree.getRootIdentifier().equals(rootIdentifierSubtreeToMerge))
-                        .collect(Collectors.toList());
-                    if (filtered != null && !filtered.isEmpty()) {
-                        matchedTree = filtered.get(0);
-                    }
-                } else {
-                    Class sectionClassFromEventTree = rootIdentifierSubtreeToMerge.getSectionEventClass();
-                    Optional<SectionTree> lastTreeOfType = getSubtrees()
-                        .stream()
-                        .filter(tree -> tree.getRootIdentifier().getSectionEventClass() == sectionClassFromEventTree)
-                        .reduce((first, second) -> second);
-                    if (lastTreeOfType.isPresent()) {
-                        matchedTree = lastTreeOfType.get();
-                    } else {
-                        // todo
-                    }
-                }
+                // if yes then find either tree with same identifier or the latest one of the same type of section
+                matchedSubtree = findWitSameIdentifierOrTheLatest(rootIdentifierSubtreeToMerge);
             }
 
-            if (matchedTree == null) {
-                AbstractReport reportToAssociate;
+            // have I find any matching subtree?
+            if (matchedSubtree == null) {
+
+                // if not check if there is any associated report in the subtree to be merged
                 if (subtreeToMerge.getAssociatedReport() == null) {
-                    try {
-                        Class subTreeSectionClass = rootIdentifierSubtreeToMerge.getSectionEventClass();
-                        Constructor[] constructors = subTreeSectionClass.getConstructors();
-                        Optional<Constructor> oneParamConstructor =
-                            Arrays.stream(constructors).filter(c -> c.getParameters().length == 1).findFirst();
-                        SectionEvent section;
-                        if (oneParamConstructor.isPresent()) {
-                             section = (SectionEvent) oneParamConstructor.get().newInstance(null);
-                        } else {
-                            section = (SectionEvent) subTreeSectionClass.newInstance();
-                        }
-                        AbstractReport dummyReport = (AbstractReport) section.getReportTypeClass().newInstance();
-                        dummyReport.setName(rootIdentifierSubtreeToMerge.getSectionId());
-                        subtreeToMerge.setAssociatedReport(dummyReport);
-                        SectionTree dummyTree = subtreeToMerge.getCloneWithoutSubtrees();
-                        reportToAssociate = getAssociatedReport().addNewReport(dummyReport);
-                        getSubtrees().add(dummyTree);
-                        dummyTree.merge(subtreeToMerge);
-                    } catch (InstantiationException e) {
-                        e.printStackTrace();
-                    } catch (IllegalAccessException e) {
-                        e.printStackTrace();
-                    } catch (InvocationTargetException e) {
-                        e.printStackTrace();
-                    }
-                    // todo log
+                    // if not it means that there was expected that this tree should already exist, so the tree has to be created
+                    createMissingSubtreeAndMergeIt(rootIdentifierSubtreeToMerge, subtreeToMerge);
+
                 }else {
-                    reportToAssociate = getAssociatedReport().addNewReport(subtreeToMerge.getAssociatedReport());
+                    // if yes then the associated report is the one that has been reported, so please add it into the report associated with this tree
+                    AbstractReport reportToAssociate =
+                        getAssociatedReport().addNewReport(subtreeToMerge.getAssociatedReport());
                     subtreeToMerge.setAssociatedReport(reportToAssociate);
                     getSubtrees().add(subtreeToMerge);
                 }
             } else {
-                matchedTree.merge(subtreeToMerge);
+                //if there is matching subtree then merge it
+                matchedSubtree.merge(subtreeToMerge);
             }
         }
 
+    }
+
+    private SectionTree findWitSameIdentifierOrTheLatest(Identifier rootIdentifierSubtreeToMerge) {
+        // check if the identifier has complete information
+        if (Validate.isNotEmpty(rootIdentifierSubtreeToMerge.getSectionId())) {
+            // find subtree with the same identifier
+            List<SectionTree> filtered = getSubtrees()
+                .stream()
+                .filter(tree -> tree.getRootIdentifier().equals(rootIdentifierSubtreeToMerge))
+                .collect(Collectors.toList());
+            if (filtered != null && !filtered.isEmpty()) {
+                return filtered.get(0);
+            }
+
+        } else {
+            // find the latest tree of the same section type
+            Class sectionClassFromEventTree = rootIdentifierSubtreeToMerge.getSectionEventClass();
+            Optional<SectionTree> lastTreeOfType = getSubtrees()
+                .stream()
+                .filter(tree -> tree.getRootIdentifier().getSectionEventClass() == sectionClassFromEventTree)
+                .reduce((first, second) -> second);
+            if (lastTreeOfType.isPresent()) {
+                return lastTreeOfType.get();
+            }
+        }
+        return null;
+    }
+
+    private void createMissingSubtreeAndMergeIt(Identifier rootIdentifierSubtreeToMerge, SectionTree subtreeToMerge) {
+        try {
+            // get class of the missing section
+            Class subTreeSectionClass = rootIdentifierSubtreeToMerge.getSectionEventClass();
+            // and use non-parametric constructor to construct
+            SectionEvent dummySection = (SectionEvent) subTreeSectionClass.newInstance();
+            // from created dummy section get class of payload and create an instance
+            AbstractReport dummyReport = (AbstractReport) dummySection.getReportTypeClass().newInstance();
+            // set name of dummy report to the section id specified in identifier
+            dummyReport.setName(rootIdentifierSubtreeToMerge.getSectionId());
+            // set dummy report as an associated report
+            subtreeToMerge.setAssociatedReport(dummyReport);
+            // and also into the current report structure
+            getAssociatedReport().addNewReport(dummyReport);
+            // crated a clone of the missing tree without any subtree
+            SectionTree dummyTree = subtreeToMerge.getCloneWithoutSubtrees();
+            // add the dummy tree
+            getSubtrees().add(dummyTree);
+            // and merge the subtree with the dummy tree
+            dummyTree.merge(subtreeToMerge);
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+            // todo
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+            // todo
+        }
+        // todo log
     }
 }
