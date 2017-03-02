5f691564114313825bdb7c35a6dec95df4d88207
Alex Soto <asotobu@gmail.com>
2017-03-02 11:26:34 +0100

    Adds FileEntry implementation (#4)
    * Adds FileEntry implementation
    
    * Sets String as File path type
    
    * Changes attribute name

--- core/reporter-api/src/main/java/org/arquillian/reporter/api/model/entry/FileEntry.java
+++ core/reporter-api/src/main/java/org/arquillian/reporter/api/model/entry/FileEntry.java
@@ -1,9 +1,61 @@
 package org.arquillian.reporter.api.model.entry;
 
+import java.io.File;
+import java.nio.file.Path;
+
 /**
  * An {@link Entry} representing a file
  *
  * @author <a href="mailto:mjobanek@redhat.com">Matous Jobanek</a>
  */
 public class FileEntry implements Entry {
+
+    private String filePath;
+
+    /**
+     * Build object by setting file path to Path.toString().
+     * @param path file.
+     */
+    public FileEntry(Path path) {
+        this.filePath = path.toString();
+    }
+
+    /**
+     * Build object by setting file path.
+     * @param path file.
+     */
+    public FileEntry(String path) {
+        this.filePath = path;
+    }
+
+    /**
+     * Build object by setting file path to File.getPath().
+     * @param path file.
+     */
+    public FileEntry(File path) {
+        this.filePath = path.getPath();
+    }
+
+    public String getPath() {
+        return filePath;
+    }
+
+    public void setPath(String path) {
+        this.filePath = path;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        FileEntry fileEntry = (FileEntry) o;
+
+        return filePath != null ? filePath.equals(fileEntry.filePath) : fileEntry.filePath == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return filePath != null ? filePath.hashCode() : 0;
+    }
 }
