821411a4a367d25fde35a5aafe7725c6725c08c8
hemanik <hemanikatyal@gmail.com>
2017-03-30 14:38:31 +0530

    Adds simple ftest.

---
+++ ftest/src/main/java/Greeter.java
@@ -0,0 +1,13 @@
+import java.io.PrintStream;
+
+public class Greeter {
+    public void greet(PrintStream to, String name) {
+
+        to.println(createGreeting(name));
+    }
+
+    public String createGreeting(String name) {
+
+        return "Hello, " + name + "!";
+    }
+}
