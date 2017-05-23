742ee516df2ccec7d05e5272bd303b3e5f0a959f
Hemani <hemanikatyal@gmail.com>
2017-05-24 03:04:40 +0530

    Adds test for standalone section. (#22)
    1. Verifies standalone reports are created and not merged.
    2. Verifies section tree has no record for the event.

--- core/reporter-impl/src/test/java/org/arquillian/reporter/impl/section/TestClassSectionTreeEventManagerTest.java
+++ core/reporter-impl/src/test/java/org/arquillian/reporter/impl/section/TestClassSectionTreeEventManagerTest.java
@@ -2,23 +2,28 @@ package org.arquillian.reporter.impl.section;
 
 import java.util.List;
 import java.util.Map;
-
 import org.arquillian.reporter.api.event.SectionEvent;
+import org.arquillian.reporter.api.event.TestClassConfigurationSection;
 import org.arquillian.reporter.api.event.TestSuiteSection;
 import org.arquillian.reporter.api.model.report.ConfigurationReport;
+import org.arquillian.reporter.api.model.report.Report;
 import org.arquillian.reporter.api.model.report.TestClassReport;
 import org.arquillian.reporter.api.model.report.TestSuiteReport;
 import org.arquillian.reporter.impl.ExecutionSection;
 import org.arquillian.reporter.impl.ExecutionStore;
+import org.arquillian.reporter.impl.SectionEventManager;
+import org.arquillian.reporter.impl.utils.ReportGeneratorUtils;
 import org.junit.Test;
 
 import static org.arquillian.reporter.impl.asserts.ExecutionReportAssert.assertThatExecutionReport;
+import static org.arquillian.reporter.impl.asserts.ReportAssert.assertThatReport;
 import static org.arquillian.reporter.impl.asserts.SectionTreeAssert.assertThatSectionTree;
 import static org.arquillian.reporter.impl.utils.SectionGeneratorUtils.EXPECTED_NUMBER_OF_SECTIONS;
 import static org.arquillian.reporter.impl.utils.SectionGeneratorUtils.feedWithTestClassConfigurationSections;
 import static org.arquillian.reporter.impl.utils.SectionGeneratorUtils.feedWithTestClassSections;
 import static org.arquillian.reporter.impl.utils.SectionGeneratorUtils.feedWithTestSuiteSections;
 import static org.arquillian.reporter.impl.utils.SectionGeneratorUtils.getSubsectionsOfSomeSection;
+import static org.arquillian.reporter.impl.utils.SectionGeneratorUtils.getTestSuiteSectionName;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
@@ -54,7 +59,7 @@ public class TestClassSectionTreeEventManagerTest {
             feedWithTestClassSections(executionStore, getSubsectionsOfSomeSection(ExecutionSection.class, sections)));
         sections.putAll(
             feedWithTestClassConfigurationSections(executionStore,
-                                                   getSubsectionsOfSomeSection(TestSuiteSection.class, sections)));
+                getSubsectionsOfSomeSection(TestSuiteSection.class, sections)));
 
         int parentCount = 1 + EXPECTED_NUMBER_OF_SECTIONS * 2;
         int treeNodesCount = (int) (parentCount + Math.pow(EXPECTED_NUMBER_OF_SECTIONS, 2));
@@ -62,7 +67,7 @@ public class TestClassSectionTreeEventManagerTest {
 
         assertThatExecutionReport(executionStore.getExecutionReport())
             .reportSubtreeConsistOfGeneratedReports(TestSuiteReport.class, TestClassReport.class,
-                                                    ConfigurationReport.class);
+                ConfigurationReport.class);
 
         assertThatSectionTree(executionStore.getSectionTree())
             .wholeTreeConsistOfCouplesMatching(sections)
@@ -70,4 +75,44 @@ public class TestClassSectionTreeEventManagerTest {
             .associatedReport();
     }
 
+    @Test
+    public void testAddingStandaloneTestClassConfigurationSectionsWithReportsUsingEventManager() throws Exception {
+        ExecutionStore executionStore = new ExecutionStore();
+        Map<SectionEvent, List<? extends SectionEvent>> sections = feedWithTestSuiteSections(executionStore);
+        sections.putAll(
+            feedWithTestClassSections(executionStore, getSubsectionsOfSomeSection(ExecutionSection.class, sections)));
+        sections.putAll(
+            feedWithTestClassConfigurationSections(executionStore,
+                getSubsectionsOfSomeSection(TestSuiteSection.class, sections)));
+
+        ConfigurationReport configurationReport =
+            ReportGeneratorUtils.prepareReport(ConfigurationReport.class, "Standalone config report", 0, 10);
+
+        TestClassConfigurationSection standaloneTestClassConfig = TestClassConfigurationSection
+            .standalone(configurationReport, getClass().getEnclosingClass(), getTestSuiteSectionName(EXPECTED_NUMBER_OF_SECTIONS -2));
+
+        SectionEventManager.processEvent(standaloneTestClassConfig, executionStore);
+
+        int parentCount = 1 + EXPECTED_NUMBER_OF_SECTIONS * 2;
+        int treeNodesCount = (int) (parentCount + Math.pow(EXPECTED_NUMBER_OF_SECTIONS, 2));
+        assertThat(sections).hasSize(parentCount);
+
+        assertThatExecutionReport(executionStore.getExecutionReport())
+            .reportSubtreeConsistOfGeneratedReports(TestSuiteReport.class, TestClassReport.class);
+
+        List<TestSuiteReport> testSuiteReports = executionStore.getExecutionReport().getTestSuiteReports();
+        TestSuiteReport testSuiteReport = testSuiteReports.get(testSuiteReports.size() - 2);
+        List<TestClassReport> testClassReports = testSuiteReport.getTestClassReports();
+        TestClassReport testClassReport = testClassReports.get(testClassReports.size() - 1);
+
+        assertThatReport(testClassReport.getConfiguration()).hasNumberOfSubReports(5);
+
+        Report testClassConfigReport = testClassReport.getConfiguration().getSubReports().get(4);
+        assertThat(testClassConfigReport).isEqualTo(configurationReport);
+
+        assertThatSectionTree(executionStore.getSectionTree())
+            .wholeTreeConsistOfCouplesMatching(sections)
+            .wholeTreeHasNumberOfTreeNodes(treeNodesCount)
+            .associatedReport();
+    }
 }
